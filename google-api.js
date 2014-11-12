ba92317806132b8dfeb9ba52cbceb39789310c22
Lillini Christian <c.lillini@gmail.com>
2014-11-12 17:55:39 +0100

    implementazione scraper

---
+++ google-api.js
@@ -0,0 +1,69 @@
+var http = require('request'),
+    googleSpreadsheet = require("google-spreadsheet"),
+    config = require("./config");
+    
+var quoteIndex=0;
+
+var googleSheetUrl = function(id)
+{
+	return "https://docs.google.com/spreadsheets/d/"+id+"/gviz/tq?gid=2";
+}
+
+module.exports = {
+		getDataTable : function(id,callback)
+		{
+			var url = googleSheetUrl(id);
+			http(url, function (error, response, body) {
+				  if (!error && response.statusCode == 200) {
+				    var jsonpData = body;
+				    var json;
+				    //if you don't know for sure that you are getting jsonp, then i'd do something like this
+				    try
+				    {
+				       json = JSON.parse(jsonpData);
+				    }
+				    catch(e)
+				    {
+				        var startPos = jsonpData.indexOf('({');
+				        var endPos = jsonpData.indexOf('})');
+				        var jsonString = jsonpData.substring(startPos+1, endPos+1);
+				        json = JSON.parse(jsonString);
+				    }
+				    callback(null, json.table);
+				  } else {
+				    callback(error);
+				  }
+				})
+		},
+		writeRows : function(id,rows)
+		{
+			var my_sheet = new googleSpreadsheet(id);
+			my_sheet.setAuth(config.googleUsername,config.googlePassword, function(err){
+				for (var i=0;i<rows.length;i++)
+					{
+						if(rows[i].quote)
+							{
+							console.log(quoteIndex+" push to sheet : "+rows[i].quote);
+							quoteIndex++;
+							my_sheet.addRow(1,rows[i]);
+							}
+					}
+			    });
+		},
+		clearWorksheet : function(id)
+		{
+			var my_sheet = new googleSpreadsheet(id);
+			my_sheet.setAuth(config.googleUsername,config.googlePassword, function(err){
+			    my_sheet.getInfo( function( err, sheet_info ){
+			        console.log( sheet_info.title + ' is loaded' );
+			        // use worksheet object if you want to forget about ids
+			        sheet_info.worksheets[0].getRows( function( err, rows ){
+			            for(var i=0;i<rows.length;i++)
+			            	{
+			            		rows[i].del();
+			            	};
+			        })
+			    });
+			})
+		}
+}
--- server.js
+++ server.js
@@ -1,4 +1,65 @@
 #!/bin/env node
-//  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
+//OpenShift sample Node application
+var config = require('./config'),
+googleApi = require('./google-api'),
+wikiquoteApi = require('./wikiquote-api');
+
+console.log("Start scraper...");
+
+console.log("Clear file");
+googleApi.clearWorksheet(config.scraperOutputId);
+
+googleApi.getDataTable(config.scraperInputId,
+		function(error,data){
+	if (error)
+		console.log(error);
+	else
+	{
+		for(var i=0;i<data.rows.length;i++)
+		{
+			var title = data.rows[i].c[0].v;
+			if (title)
+			{
+			wikiquoteApi.queryTitles(title,
+					function (pageId)
+					{
+						wikiquoteApi.getSectionsForPage(pageId,
+								function(result){
+									for(var j=0;j<result.sections.length;j++)
+									{
+										wikiquoteApi.getQuotesForSection(pageId,result.sections[j],
+												function(result){
+													if (result.quotes.length>0)
+														{
+														console.log(result.quotes);
+														var tag;
+														for(var n=0;n<data.rows.length;n++)
+														{
+															if (data.rows[n].c[0].v===result.quotes[0].author)
+																tag = data.rows[n].c[2].v;
+														}
+														for(var k=0;k<result.quotes.length;k++)
+																{
+																	result.quotes[k].tag=tag;
+																}
+														console.log(result.quotes);
+														googleApi.writeRows(config.scraperOutputId,result.quotes);
+														}
+										},function(error){
+											console.log(error);
+										})
+									}
+								},function (error)
+								{
+									console.log(success);
+								})
+					},
+					function (error)
+					{
+						console.log(success);
+					});
+			//console.log(row.c[0]);
+			}
+		}
+	}
+});
